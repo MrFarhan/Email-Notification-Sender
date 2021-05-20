c20503d2414c63168f0a3eaeff3bd788aa29d175
MrFarhan <muhammadfarhan012345@gmail.com>
2021-05-20 23:26:55 +0500

    server set up done ,email console will be logged at mentioned time

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const firebase = require("firebase")
 const express = require('express')
 const app = express()
 const axios = require("axios")
@@ -8,6 +9,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors")
 app.use(cors())
 app.use(express.json());
+require('dotenv').config()
 
 var currentYear = new Date().getFullYear()
 var currentMonth = new Date().getMonth();
@@ -18,9 +20,46 @@ console.log("today is date ", today)
 // let todayDay = moment(today).format('dddd')
 // console.log("today is ", todayDay)
 
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+};
+
+firebase.initializeApp(firebaseConfig)
+
+var temp = -1
 
 async function test() {
+    schedule.scheduleJob('50 14 23 * * *', function () {
 
+    firebase.database().ref("/Attendance").on("value", (res) => {
+        const attendanceObj = res.val() // complete attendance data
+        firebase.database().ref("/Users").on("value", (res) => {
+            const users = res.val() //all users data
+            var userUids = [];
+            Object.values(users).map((item, index) => {
+                if (item.isVerified && item.role === "Authorized") {
+                    userUids.push(item.uid) // filtered verified and excluded admin /users data
+                }
+            })
+            userUids && userUids.map((uid, index) => {
+                temp = ++temp;
+                if (attendanceObj[uid] && attendanceObj[uid][currentYear] && attendanceObj[uid][currentYear][currentMonth] && (attendanceObj[uid][currentYear][currentMonth][today]) && temp > userUids.length) {
+                     console.log("present people are ",uid, "while temp is ", temp, "and total users are ", userUids.length)
+                } else {
+                    console.log("Absent users are", uid, "while temp is ", temp, "and total users are ", userUids.length)
+                }
+            })
+        })
+    })
+
+})
     // const transporter = nodemailer.createTransport({
     //     host: 'smtp.gmail.com',
     //     port: 465,
@@ -42,36 +81,11 @@ async function test() {
     //     <br/>
     //     <br/>
     //     REGARDS, <br/>
-    //     COMPUTING YARD <br/>
-    //     ATTENDANCE MANAGEMENT SYSTEM <br/>
+    //     Your company name <br/>
+    //     App / user name etc <br/>
     //     `, // html body
     // }
 
-
-    // //  working for late 12:05 pm
-    // schedule.scheduleJob('05 07 * * *', function () {
-    //     console.log('API called for Marking late')
-
-    //     transporter.sendMail(mailOption, function (err, data) {
-    //         if (err) {
-    //             console.log("error", err)
-    //         } else console.log("Email sent")
-    //     })
-    // })
-
-    //  working for late 12:05 pm
-    schedule.scheduleJob('25 47 00 * * *', function () {
-        return (console.log('TESTING TESTING TESTING AT 12:0'))
-
-
-    })
-
-
-    // //  working for Holiday at 05 17 means 10:05 my heroku
-    // schedule.scheduleJob('05 17 * * *', async function () {
-    //     console.log('API called for Marking late')
-
-    // })
 
 }
 
@@ -80,9 +94,9 @@ test().catch((e) => console.log("1st error is ", e));
 app.get("/", (req, res) => {
     async function isodate() {
         await axios.get('http://worldclockapi.com/api/json/utc/now').then((response) => {
-            // const time = moment(response.data["currentDateTime"]).format("dddd, MMMM Do YYYY, h:mm")
-            console.log("time is ", response.data["currentDateTime"])
-            return res.send(response.data["currentDateTime"])
+            const time = moment(response.data["currentDateTime"]).format("dddd, MMMM Do YYYY, h:mm")
+            console.log("time is ", time)
+            return res.send(time)
 
         }).catch((error) => {
             console.log("error on worldclock api is ", error)
